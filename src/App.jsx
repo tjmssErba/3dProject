b2169bf69de83227831248bd203bb07a5f0783ad
Erbol <erbol092007@gmail.com>
2024-03-01 18:01:20 +0600

    3dErbobo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css"
+
+
+const App=()=>{
+    console.log("rtertertretert")
+    return(
+        <div>
+         <h1 className="text-3xl font-bold underline text-red-500">Ervovo</h1>
+        </div>
+    )
+}
+export default App
